043c67f83e0461f11e5351e55b201b68dda9331d
Zhuo Chen <chenzhuo101@gmail.com>
2020-05-04 11:12:21 -0700

    fix the path issue

--- asr/scripts/gen_asrinput_baseline.sh
+++ asr/scripts/gen_asrinput_baseline.sh
@@ -4,7 +4,7 @@ PYTHON=python
 
 # args
 prep_script=./python/gen_asrinput_baseline.py
-libricss_path=/data/tayoshio/LibriCSS/orig
+libricss_path=/data/zhuc/libricss
 script_path=./scripts
 
 $PYTHON $prep_script --data_path $libricss_path --tool_path $script_path
