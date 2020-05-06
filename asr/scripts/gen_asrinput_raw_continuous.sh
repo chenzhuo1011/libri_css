923d3291c6d08b34577db66a05e355d92cdffbff
Takuya Yoshioka <takuya.yoshioka@hotmail.co.jp>
2020-05-06 12:38:54 -0700

    Remove abs path fro gen_asrinput_raw_continuous.sh

--- asr/scripts/gen_asrinput_raw_continuous.sh
+++ asr/scripts/gen_asrinput_raw_continuous.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-PYTHON=python
-
 # args
-prep_script=../python/gen_asrinput_raw_continuous.py
-libricss_path=/data/zhuc/libricss/for_release
-script_path=../scripts
+thisdir=`dirname $0`
+thisdir=`realpath $thisdir`
+
+prep_script=$thisdir/../python/gen_asrinput_raw_continuous.py
+libricss_path=$EXPROOT/data
+
+script_path=$thisdir
 asr_path=/data/zhuc/libricss/opencss
 
 echo "$prep_script --data_path $libricss_path --tool_path $script_path --asr_path $asr_path"
-
-$PYTHON $prep_script --data_path $libricss_path --tool_path $script_path --asr_path $asr_path
-
+python $prep_script --data_path $libricss_path --tool_path $script_path --asr_path $asr_path
