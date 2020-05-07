7340caa7f3f2041a0f7b006cbc05d16cba4a6de9
Takuya Yoshioka <takuya.yoshioka@hotmail.co.jp>
2020-05-06 18:29:04 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,28 @@ SCTKPATH=external_tools/sctk
 PYKALDIPATH=external_tools/pykaldi2
 
 
+# Check if tools are installed.
+if ! which wget >/dev/null; then
+    echo "$0: wget is not installed."
+    exit 1;
+fi
+if ! which realpath >/dev/null; then
+    echo "$0: realpath is not installed."
+    exit 1;
+fi
+if ! which docker >/dev/null; then
+    echo "$0: docker is not installed."
+    exit 1;
+fi
+if ! which git >/dev/null; then
+    echo "$0: git is not installed."
+    exit 1;
+fi
+
+
 ##
 ## Installing SCTK. 
 ##
-
 if [[ ! -d $SCTKPATH ]]
 then
     read -p "Installing SCTK in $SCTKPATH. Hit Enter or type an alternative install path name: " ret
@@ -51,19 +69,16 @@ then
         CWD=`pwd`
         git clone https://github.com/jzlianglu/pykaldi2.git -b libcss $PYKALDIPATH
         cd $PYKALDIPATH
-        sudo docker pull pykaldi2docker/horovod-pykaldi:libcss.v1.0
+        docker pull pykaldi2docker/horovod-pykaldi:libcss.v1.0
         cd $CWD
     else
         echo "$PYKALDIPATH exists. Skip PyKaldi2 installation."    
     fi
     
-  cd $PYKALDIPATH
-
-
-   wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Xon9jS2vEDOcwYAMrN2XAMrL-7PE2qXj' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1Xon9jS2vEDOcwYAMrN2XAMrL-7PE2qXj" -O libri_AM.zip && rm -rf /tmp/cookies.txt
-    # unzip
-  unzip libri_AM.zip
-  cd $CWD  
+    cd $PYKALDIPATH
+    wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Xon9jS2vEDOcwYAMrN2XAMrL-7PE2qXj' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1Xon9jS2vEDOcwYAMrN2XAMrL-7PE2qXj" -O libri_AM.zip && rm -rf /tmp/cookies.txt
+    unzip libri_AM.zip
+    cd $CWD  
 fi
 
 ##
@@ -73,10 +88,9 @@ SCTKPATH=`realpath $SCTKPATH`
 echo "export SCTKPATH=$SCTKPATH" > path.sh 
 PYKALDIPATH=`realpath $PYKALDIPATH`
 echo "export PYKALDIPATH=$PYKALDIPATH" >> path.sh
-
-echo "export AMPATH=$AMPATH/AM" >> path.sh
-
+echo "export AMPATH=$PYKALDIPATH/AM" >> path.sh
 
 EXPROOT=`dirname $0`/exp
 EXPROOT=`realpath $EXPROOT`
 echo "export EXPROOT=$EXPROOT" >> path.sh
+
