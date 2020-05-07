ffb786f9e4c29ac5ef877662eae9a678115bbbb9
Takuya Yoshioka <takuya.yoshioka@hotmail.co.jp>
2020-05-06 18:18:26 -0700

    Update gen_asrinput_raw_continuous.sh

--- asr/scripts/gen_asrinput_raw_continuous.sh
+++ asr/scripts/gen_asrinput_raw_continuous.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
+PYTHON=python
+
 # args
 prep_script=../python/gen_asrinput_raw_continuous.py
 libricss_path=$EXPROOT/data
 script_path=../scripts
 asr_path=$AMPATH
 
-prep_script=$thisdir/../python/gen_asrinput_raw_continuous.py
-libricss_path=$EXPROOT/data
-
-script_path=$thisdir
-asr_path=/data/zhuc/libricss/opencss
-
 echo "$prep_script --data_path $libricss_path --tool_path $script_path --asr_path $asr_path"
-python $prep_script --data_path $libricss_path --tool_path $script_path --asr_path $asr_path
+
+$PYTHON $prep_script --data_path $libricss_path --tool_path $script_path --asr_path $asr_path
